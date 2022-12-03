9ed9e22081751780115fee129f3a80a8fbb07468
Gustavo Silva Quieregato <quieregatog@alunos.utfpr.edu.br>
2022-12-03 14:26:49 -0300

    Chat changed

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/share/python
+from services.server import Server
+from services.sql_connection import MysqlConnection
+
+
+def mainServer():
+    connector = MysqlConnection('127.0.0.1', 'root', 'Leontechh@15', 'chatRMI')
+    serverMain = Server('127.0.0.1', 11000, connector)
+    while True:
+        if serverMain:
+            serverMain.listenMessage()
+
+
+# Try exception error
+try:
+    mainServer()
+except TypeError as e:
+    print(e)
+    print("Verify your params")
+except KeyboardInterrupt:
+    print("\nThank you for using our app!!!")
---
+++ services/sql_connection.py
@@ -0,0 +1,47 @@
+import mysql.connector
+from mysql.connector import Error
+from datetime import datetime, timedelta
+
+from utils.functions import FunctionUtils
+
+
+class MysqlConnection:
+    def __init__(self, hostU, userU, passwordU, dbU):
+        self.connector = mysql.connector.connect(user=userU, password=passwordU, host=hostU, database=dbU)
+        self.query = ''
+        self.functionAux = FunctionUtils()
+
+    def getCursor(self):
+        return self.connector.cursor(buffered=True)
+
+    def getConnection(self):
+        return self.connector
+
+    def getDate(self):
+        return datetime.now().date() + timedelta(days=1)
+
+    def mount_query(self, table='', fields='', where=''):
+        try:
+            if fields:
+                self.query = ('select ' + fields + ' from ' + str(table) + '')
+            else:
+                self.query = ('select * from ' + str(table) + '')
+
+            if where:
+                self.query = self.query + ' WHERE ' + where + ''
+            print("Query selection com success!")
+        except Exception as e:
+            return e
+
+    def submit_query(self):
+        sql = self.getCursor()
+        try:
+            print(self.query)
+            sql.execute(self.query)
+            data = list(sql.fetchall())
+            sql.close()
+            if len(data) == 0:
+                return self.functionAux.interface_bd('')
+            return self.functionAux.interface_bd(data)
+        except Error as e:
+            return e
