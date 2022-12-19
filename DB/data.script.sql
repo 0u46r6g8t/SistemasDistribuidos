905d3778441c6754c6996ce0ccc888fa39defe7b
Rafael Omodei <rafael.omodei@dasa.com.br>
2022-12-19 13:34:06 -0300

    update

--- DB/data.script.sql
+++
@@ -1,4 +0,0 @@
-SET NAMES utf8mb4;
-
-INSERT INTO `tb_user` (`id`, `address_ip`, `name`, `image`, `password`) VALUES
-(1,	'192.168.10.1',	'Gustavo Silva Quieregato',	'assets/images01.png',	'vgpwgv32');
